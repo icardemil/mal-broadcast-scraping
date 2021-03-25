8b8a89fa1b7915c7e5002b376f2e8afe41c3c5d9
icardemil64 <icardemil64@gmail.com>
2021-03-25 00:10:17 -0300

    Agrego diccionario de series y comentarios al código

--- anime.py
+++ anime.py
@@ -11,22 +11,39 @@ import requests
 
 #days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
 days = ["monday"]
-for day in days:
-    r = requests.get('https://api.jikan.moe/v3/schedule/%s' % day)
-    if r.status_code == 200:
-        res = r.json()
-        for item in res[day]:
-            print(item["url"])
-            pageContent = requests.get(item["url"])
-            pageContent.encoding = "utf-8"
-            soup = BeautifulSoup(pageContent.text,"lxml")
-            data = soup.find("div",{"id":"content"}).find("td",{"class":"borderClass"}).findAll("div",{"class":"spaceit"})
-            print(data[2].text.split())
-    else:
-        print("Error")
+series = []
 
-# pc = requests.get('https://myanimelist.net/anime/40028/Shingeki_no_Kyojin__The_Final_Season')
-# pc.encoding = "utf-8"
-# sp = BeautifulSoup(pc.text,"lxml")
-# dt = sp.find("div",{"id":"content"}).find("td",{"class":"borderClass"}).findAll("div",{"class":"spaceit"})
-# print(dt[2].text.split())
+def getSerie(item):
+    #Inicializa el diccionario
+    serieDict = {}
+    
+    #Obitiene información y establece el DOM
+    pageContent = requests.get(item["url"])
+    pageContent.encoding = "utf-8"
+    soup = BeautifulSoup(pageContent.text,"lxml")
+    
+    #Busca la información del broadcast
+    data = soup.find("div",{"id":"content"}).find("td",{"class":"borderClass"}).findAll("div",{"class":"spaceit"})
+    #La segunda posición contiene la información del broadcast
+    info = data[2].text.split()
+    
+    #Asigna valores al diccionario
+    serieDict["title"] = item["title"]
+    serieDict["img_url"] = item["image_url"]
+    serieDict["synopsis"] = item["synopsis"]
+    serieDict["mal_url"] = item["url"]
+    serieDict["day"] = info[1]
+    serieDict["hour"] = info[3]
+    serieDict["time"] = info[4].replace('(','').replace(')','')
+    return serieDict
+
+for day in days:
+    req = requests.get('https://api.jikan.moe/v3/schedule/%s' % day)
+    if req.status_code == 200:
+        res = req.json()
+        for item in res[day]:
+            series.append(getSerie(item))
+    else:
+        print("Error en la petición")
+
+print(series)
